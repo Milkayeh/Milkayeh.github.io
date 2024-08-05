9625fb96d63cd958680adef113c47a64d9283280
LaneMilki <gendarkdragon@gmail.com>
2024-08-05 20:23:14 +0800

    Tailwind adjustments

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,33 +8,33 @@ const Header = () => {
     return (
         <header>
             <div className='py-5 px-5 md:px-12 lg:px-28'>
-                <div className='items-center text-center'> 
-                <span className="ml-3 text-2xl font-semibold">Milkayeh&#39;s Research Centre</span>  </div>
-                
-                
+                <div className='flex justify-center'>
+                    <span className="text-2xl font-semibold">Milkayeh&#39;s Research Centre</span>
+                </div>
+
                 {/* Navbar */}
-                <nav className="text-center ">
-                        <ul>
-                            <li>
-                                <a> ( </a>
-                                <Link href="/" className="hover:text-blue-600 transition-colors duration-200">Home</Link>
-                                <a> | </a>
-                                <Link href="/about" className="hover:text-blue-600 transition-colors duration-200">About Me</Link>
-                                <a> | </a>
-                                <Link href="/blog" className="hover:text-blue-600 transition-colors duration-200">Blog</Link>
-                                <a> | </a>
-                                <Link href="/contact" className="hover:text-blue-600 transition-colors duration-200">Find Me</Link>
-                                <a> ) </a>
-                            </li>
-                        </ul>
-                        
+                <nav className="text-center mt-4">
+                    <ul className="inline-flex space-x-4">
+                        <li>
+                            <Link href="/" className="hover:text-blue-600 transition-colors duration-200">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className="hover:text-blue-600 transition-colors duration-200">About Me</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog" className="hover:text-blue-600 transition-colors duration-200">Blog</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact" className="hover:text-blue-600 transition-colors duration-200">Find Me</Link>
+                        </li>
+                    </ul>
                 </nav>
 
-
-                <div style={{ display: 'flex', justifyContent: 'center' }}> <Image src={assets.main} width={180} alt='' className='w-[130px] sm:w-auto'/> </div>
+                <div className="flex justify-center mt-4">
+                    <Image src={assets.main} width={180} alt='' className='w-[130px] sm:w-auto'/>
+                </div>
                 {/* Empty div for spacing */}
-                <div className="w-50 "></div>
-                
+                <div className="w-50"></div>
             </div>
         </header>
     )
