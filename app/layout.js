3fcccbd8d3c74fbf27ef193c842ab55f921c1442
LaneMilki <gendarkdragon@gmail.com>
2024-08-05 20:40:57 +0800

    To Do List component added, adjusted a few things in layout.js, deleted un-needed test file from assets.js

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,23 @@ import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import 'highlight.js/styles/github.css'; 
 
-const roboto_Condensed = Roboto_Condensed({ subsets: ["latin"], weight:["400", "500", "600", "700"] });
+
+
+// If loading a variable font, you don't need to specify the font weight
+const roboto_condensed = Roboto_Condensed({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={roboto_condensed.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
 
 export const metadata = {
   title: "Milkayeh",
   description: "Milkayeh's blog website",
 };
-
-
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={roboto_Condensed.className}>{children}</body>
-    </html>
-  );
-}
